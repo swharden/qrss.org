fd7fd8098ab99b04354321752fb8ad7a275551df
Scott W Harden <swharden@gmail.com>
2024-12-30 18:36:12 -0500

    Create minimal API test project using Docker

---
+++ src/DevTestApi/Program.cs
@@ -0,0 +1,17 @@
+using DevTestApi.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapWeatherEndpoints();
+app.Run();
