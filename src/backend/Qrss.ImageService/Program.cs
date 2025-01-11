fba261383d68c83ea2451c685a756471031a8d27
Scott W Harden <swharden@gmail.com>
2025-01-11 12:31:52 -0500

    add logging

--- src/backend/Qrss.ImageService/Program.cs
+++ src/backend/Qrss.ImageService/Program.cs
@@ -1,19 +1,44 @@
 ﻿using Qrss.Core.Domain;
+using System.Diagnostics;
+using System.Text;
 
-Console.WriteLine($"Downloading CSV file...");
+Logger logger = new();
+
+logger.Log($"Downloading CSV file...");
 const string GrabbersCsvUrl = "https://raw.githubusercontent.com/swharden/QRSSplus/refs/heads/master/grabbers.csv";
 IGrabberInfoDB grabberDatabase = await Qrss.Core.GrabberInfoDatabases.CsvDB.FromCsvUrl(GrabbersCsvUrl);
-Console.WriteLine($"Identified {grabberDatabase.ReadAll().Count()} grabbers");
+logger.Log($"Identified {grabberDatabase.ReadAll().Count()} grabbers");
 
-Console.WriteLine($"Connecting to image database...");
+logger.Log($"Connecting to image database...");
 IGrabImageManager imageManager = new Qrss.Core.GrabImageManagers.FlatFolder("./image-db");
-Console.WriteLine($"Located {imageManager.ImageCount} existing images");
+logger.Log($"Located {imageManager.ImageCount} existing images");
 
-Console.WriteLine($"Downloading new images...");
+logger.Log($"Downloading new images...");
 await imageManager.DownloadImagesAsync(grabberDatabase);
 
-Console.WriteLine($"Deleting old images...");
+logger.Log($"Deleting old images...");
 await imageManager.DeleteOldImagesAsync(TimeSpan.FromHours(1));
 
-Console.WriteLine("DONE!");
-Console.ReadLine();
+logger.Log("DONE!");
+logger.SaveAs($"log-{DateTime.UtcNow.Ticks}.txt");
+
+class Logger()
+{
+    StringBuilder SB = new();
+    Stopwatch SW = Stopwatch.StartNew();
+
+    public void Log(string msg)
+    {
+        string timestamp = DateTime.Now.ToString();
+        string line = $"[{SW.Elapsed.TotalSeconds:N2}] {msg}";
+        Console.WriteLine(line);
+        SB.AppendLine(line);
+    }
+
+    public void SaveAs(string filename)
+    {
+        filename = Path.GetFullPath(filename);
+        File.WriteAllText(filename, SB.ToString());
+        Console.WriteLine($"Wrote: {filename}");
+    }
+}
