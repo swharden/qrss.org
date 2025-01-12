5d232a62944efd6bb308f3f719ed734a1923e678
Scott W Harden <swharden@gmail.com>
2025-01-11 19:37:13 -0500

    ImageService: download every 10 minutes

--- src/backend/Qrss.ImageService/Program.cs
+++ src/backend/Qrss.ImageService/Program.cs
@@ -2,35 +2,72 @@
 using System.Diagnostics;
 using System.Text;
 
-string dataFolder = Path.Combine(AppContext.BaseDirectory, "data");
-if (Directory.Exists(dataFolder))
+const int RUN_MINUTE_ONES_DIGIT = 2;
+
+int lastRunMinute = -1;
+int checks = 0;
+int runs = 0;
+
+while (true)
 {
-    Directory.CreateDirectory(dataFolder);
+    int thisMinute = DateTime.UtcNow.Minute;
+    int minuteOnes = thisMinute % 10;
+
+    // TODO: replace this with improved analytics
+    Console.Write($"{DateTime.UtcNow} minute: {minuteOnes} checks:{checks++} runs:{runs}\r");
+    Thread.Sleep(1000);
+
+    bool isRunMinute = minuteOnes == RUN_MINUTE_ONES_DIGIT;
+    bool runExecutedPreviously = thisMinute == lastRunMinute;
+    bool timeForNextRun = isRunMinute && !runExecutedPreviously;
+    bool isFirstRun = runs == 0;
+
+    if (isFirstRun || timeForNextRun)
+    {
+        lastRunMinute = thisMinute;
+        runs += 1;
+        DownloadNewImages();
+        Console.WriteLine();
+    }
 }
-Console.WriteLine($"APP FOLDER: {dataFolder}");
 
-Logger logger = new();
+static void DownloadNewImages()
+{
+    DownloadNewImagesAsync().GetAwaiter().GetResult();
+}
 
-logger.Log($"Downloading CSV file...");
-const string GrabbersCsvUrl = "https://raw.githubusercontent.com/swharden/QRSSplus/refs/heads/master/grabbers.csv";
-IGrabberInfoDB grabberDatabase = await Qrss.Core.GrabberInfoDatabases.CsvDB.FromCsvUrl(GrabbersCsvUrl);
-logger.Log($"Identified {grabberDatabase.ReadAll().Count()} grabbers");
+static async Task DownloadNewImagesAsync()
+{
+    string dataFolder = Path.Combine(AppContext.BaseDirectory, "data");
+    if (Directory.Exists(dataFolder))
+    {
+        Directory.CreateDirectory(dataFolder);
+    }
+    Console.WriteLine($"APP FOLDER: {dataFolder}");
 
-logger.Log($"Connecting to image database...");
-string imageFolder = Path.Combine(dataFolder, "./grabs");
-IGrabImageManager imageManager = new Qrss.Core.GrabImageManagers.FlatFolder(imageFolder);
-logger.Log($"Located {imageManager.ImageCount} existing images");
+    Logger logger = new();
 
-logger.Log($"Downloading new images...");
-await imageManager.DownloadImagesAsync(grabberDatabase);
+    logger.Log($"Downloading CSV file...");
+    const string GrabbersCsvUrl = "https://raw.githubusercontent.com/swharden/QRSSplus/refs/heads/master/grabbers.csv";
+    IGrabberInfoDB grabberDatabase = await Qrss.Core.GrabberInfoDatabases.CsvDB.FromCsvUrl(GrabbersCsvUrl);
+    logger.Log($"Identified {grabberDatabase.ReadAll().Count()} grabbers");
 
-logger.Log($"Deleting old images...");
-await imageManager.DeleteOldImagesAsync(TimeSpan.FromHours(1));
+    logger.Log($"Connecting to image database...");
+    string imageFolder = Path.Combine(dataFolder, "./grabs");
+    IGrabImageManager imageManager = new Qrss.Core.GrabImageManagers.FlatFolder(imageFolder);
+    logger.Log($"Located {imageManager.ImageCount} existing images");
 
-logger.Log("DONE!");
+    logger.Log($"Downloading new images...");
+    await imageManager.DownloadImagesAsync(grabberDatabase);
 
-string logFolder = Path.Combine(dataFolder, "./logs");
-logger.SaveAs(logFolder);
+    logger.Log($"Deleting old images...");
+    await imageManager.DeleteOldImagesAsync(TimeSpan.FromHours(1));
+
+    logger.Log("DONE!");
+
+    string logFolder = Path.Combine(dataFolder, "./logs");
+    logger.SaveAs(logFolder);
+}
 
 class Logger()
 {
